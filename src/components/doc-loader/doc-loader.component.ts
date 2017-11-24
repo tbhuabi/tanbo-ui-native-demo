fcb57d5cab6f74a20faca9031c2a3d6ec144727c
tanbo <464328895@qq.com>
2017-11-24 21:41:07 +0800

    更新文档，完成tab

--- src/components/doc-loader/doc-loader.component.ts
+++ src/components/doc-loader/doc-loader.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, Event, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'doc-loader',
     templateUrl: './doc-loader.component.html'
 })
-export class DocLoaderComponent implements OnInit {
-    constructor(private activatedRoute: ActivatedRoute) {
+export class DocLoaderComponent implements OnInit, OnDestroy {
+    sub: Subscription;
+
+    constructor(private activatedRoute: ActivatedRoute,
+                private router: Router) {
     }
 
     ngOnInit() {
@@ -15,5 +19,20 @@ export class DocLoaderComponent implements OnInit {
         if (parent) {
             parent.postMessage(data, '*');
         }
+        this.sub = this.router.events.subscribe((e: Event) => {
+            if (e instanceof NavigationEnd) {
+                const url = '/' + this.activatedRoute.snapshot.url.join('/');
+                const reg = new RegExp('^' + url);
+                console.log(e.url);
+                if (reg.test(e.url) && parent) {
+                    console.log(true)
+                    // parent.postMessage(data, '*');
+                }
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 }
